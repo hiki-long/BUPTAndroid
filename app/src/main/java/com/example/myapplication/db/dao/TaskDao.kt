d1516e8c403f24304b0bc31203274767520dbc7b
wjl <411046642@qq.com>
2021-04-17 16:37:43 +0800

    修改数据库表字段表示task_id改为todo_id

--- app/src/main/java/com/example/myapplication/db/dao/TaskDao.kt
+++ app/src/main/java/com/example/myapplication/db/dao/TaskDao.kt
@@ -6,27 +6,27 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
-    @Query("DELETE FROM task WHERE task_id = :id")
+    @Query("DELETE FROM task WHERE todo_id = :id")
     fun deleteTask(id : Int)
 
-    @Query("SELECT * FROM task WHERE task_id = :id")
+    @Query("SELECT * FROM task WHERE todo_id = :id")
     fun getTask(id: Int): Flow<TaskEntity>
 
-    @Query("SELECT * FROM task ORDER BY todo_create_time")
+    @Query("SELECT * FROM task ORDER BY datetime(todo_create_time)")
     fun getTasks(): Flow<List<TaskEntity>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertTask(task: TaskEntity): Long
 
     //更新task状态
-    @Query("UPDATE task SET todo_state = 'DOING' WHERE task_id = :id")
+    @Query("UPDATE task SET todo_state = 'DOING' WHERE todo_id = :id")
     fun setTaskDoing(id: Int)
 
-    @Query("UPDATE task SET todo_state = 'DONE' WHERE task_id = :id")
+    @Query("UPDATE task SET todo_state = 'DONE' WHERE todo_id = :id")
     fun setTaskDone(id: Int)
 
     //更新task名字
-    @Query("UPDATE task SET todo_name = :name WHERE task_id = :id")
+    @Query("UPDATE task SET todo_name = :name WHERE todo_id = :id")
     fun setTaskName(id: Int, name: String)
 
     @Update
