cf8e942a696e7933bf97850c437c251688a520c8
wjl <411046642@qq.com>
2021-04-17 23:37:55 +0800

    找好了颜色选择器的轮子，并且能够正确使用

---
+++ app/src/main/java/com/example/myapplication/ui/todo/ListDialogCreate.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication.ui.todo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.myapplication.R
+
+class ListDialogCreate : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.listdialog, container, false)
+    }
+
+}
--- app/src/main/java/com/example/myapplication/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/todo/TodoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.todo
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +9,19 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.MainActivity
 import com.example.myapplication.R
+import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog
+import com.github.dhaval2404.colorpicker.model.ColorShape
+import com.github.dhaval2404.colorpicker.model.ColorSwatch
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class TodoFragment : Fragment() {
 
     private lateinit var todoViewModel: TodoViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,6 +34,21 @@ class TodoFragment : Fragment() {
         todoViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        val bt: FloatingActionButton = root.findViewById(R.id.add)
+        bt.setOnClickListener {
+//            var dialog = ListDialogCreate()
+//            dialog.show(parentFragmentManager,"listdialog")
+            MaterialColorPickerDialog
+                .Builder((activity as MainActivity?)!!)        					// Pass Activity Instance
+                .setTitle("Pick Theme")           		// Default "Choose Color"
+                .setColorShape(ColorShape.SQAURE)   	// Default ColorShape.CIRCLE
+                .setColorSwatch(ColorSwatch._300)   	// Default ColorSwatch._500
+                .setDefaultColor("#F5F5F5") 		// Pass Default Color
+                .setColorListener { color, colorHex ->
+                    // Handle Color Selection
+                }
+                .show()
+        }
         return root
     }
 }
