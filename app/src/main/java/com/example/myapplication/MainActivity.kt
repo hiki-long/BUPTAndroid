43c4fa97b668acd1ff0d831d9335d0359adbb910
Rockey <1350291943@qq.com>
2021-04-19 22:49:48 +0800

    增加todo的侧滑栏和侧滑菜单

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -23,5 +26,22 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_todo, R.id.navigation_view, R.id.navigation_course, R.id.navigation_more))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        toolbar.setNavigationOnClickListener{
+            //还需加上判断是否在todo fragment的逻辑!!
+            mainDrawerLayout.openDrawer(GravityCompat.START)
+        }
+        supportActionBar?.let{
+            //还需加上判断是否在todo fragment的逻辑!!
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home->mainDrawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true;
     }
 }
