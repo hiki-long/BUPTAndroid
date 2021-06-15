395349aa7f289a915a4002025706cb7c3fef684d
reecewang <928082159@qq.com>
2021-06-15 12:45:48 +0800

    完善更多页面

--- app/src/main/java/com/example/myapplication/ui/more/MoreFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/more/MoreFragment.kt
@@ -57,22 +57,10 @@ class MoreFragment : Fragment() {
      *
      * @param key 由官网生成的key
      * @return 返回true表示呼起手Q成功，返回false表示呼起失败
-//     ******************/
-//    public boolean joinQQGroup(String key) {
-//        Intent intent = new Intent();
-//        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26jump_from%3Dwebapi%26k%3D" + key));
-//        // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//        try {
-//            startActivity(intent);
-//            return true;
-//        } catch (Exception e) {
-//            // 未安装手Q或安装的版本不支持
-//            return false;
-//        }
-//    }
-    public fun joinQQGroup (key : String) :Boolean{
+     ******************/
+    private fun joinQQGroup (key : String) :Boolean{
         val intent = Intent()
-        intent.setData(Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26jump_from%3Dwebapi%26k%3D" + key));
+        intent.data = Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26jump_from%3Dwebapi%26k%3D$key");
         // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面    //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         return try {
             startActivity(intent);
